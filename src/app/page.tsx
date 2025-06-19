cb238693447a2f5e8bedfe46083b3ff65c16c77e
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-19 17:52:03 +0545

    hot fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-[686px] 2xl:h-[902px] rounded-b-md flex flex-col pt-20 gap-2 items-center text-48px 2xl:text-[52px] w-full bg-gray-300">
-      Box width - 1536px
+    <div className="h-[calc(100vh-52px)] rounded-b-md flex flex-col pt-20 gap-2 items-center text-48px 2xl:text-[44px] w-full bg-gray-300">
+      Box width - 1536px (i.e 125% scale of 1920)
       <div className="flex flex-col gap-1 items-center 2xl:hidden">
         <p>Hello from Home </p>
         <p>Text Size - 48px</p>
-        <p>Overall Screen Height for 1536px (i.e 125% scale of 1920) - 686px</p>
       </div>
       <div className="hidden flex-col gap-1 items-center 2xl:flex">
         <p>Hello from Home </p>
         <p>Text Size - 52px</p>
-        <p>Overall Screen Height for 1920px (i.e 100% scale of 1920) - 954px</p>
       </div>
     </div>
   );
