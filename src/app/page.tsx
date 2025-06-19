2f6a237b86325a8cc7caa03d6e50659bbff49342
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-19 17:54:36 +0545

    hot fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="h-[calc(100vh-52px)] rounded-b-md flex flex-col pt-20 gap-2 items-center text-48px 2xl:text-[44px] w-full bg-gray-300">
-      Box width - 1536px (i.e 125% scale of 1920)
+      Box width - 1536px
       <div className="flex flex-col gap-1 items-center 2xl:hidden">
         <p>Hello from Home </p>
         <p>Text Size - 48px</p>
