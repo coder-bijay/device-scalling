1bd6ca81bf323a139632be0d6306d4fda09eca6b
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-19 17:38:18 +0545

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Container from "@/Components/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Container>
+          <div className="h-[52px] flex justify-center items-center rounded-t-md bg-black text-white shadow-xl">
+            height-52px (Navbar)
+          </div>
+          {children}
+        </Container>
       </body>
     </html>
   );
