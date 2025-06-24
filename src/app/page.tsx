977166a6839d0727dcc6850481bc6fbe3ff71094
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-24 10:09:17 +0545

    feat: added the 742px height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Home = () => {
+  // h-[calc(100vh-52px)]
   return (
-    <div className="h-[calc(100vh-52px)] rounded-b-md flex flex-col pt-20 gap-2 items-center text-48px 2xl:text-[44px] w-full bg-gray-300">
+    <div className=" h-[690px] rounded-b-md flex flex-col pt-20 gap-2 items-center text-48px 2xl:text-[44px] w-full bg-gray-300">
       Box width - 1536px
       <div className="flex flex-col gap-1 items-center 2xl:hidden">
         <p>Hello from Home </p>
@@ -12,6 +13,7 @@ const Home = () => {
         <p>Hello from Home </p>
         <p>Text Size - 44px</p>
       </div>
+      overall height - 742px
     </div>
   );
 };
